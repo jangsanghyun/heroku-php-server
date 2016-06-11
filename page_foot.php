dc05882be0edd5e685592b709771ab25f66d6c35
jangsang <episodejsh@naver.com>
2016-06-12 00:10:57 +0900

    main finished

--- page_foot.php
+++ page_foot.php
@@ -1,19 +1,27 @@
 
 
 	<div class="footer">
-		<div style="display:table; margin:0 auto; width:90%; height:auto; border-bottom:1px solid #3e3e3e;">
+		<div style="display: table;
+    margin: 0 auto;
+    width: 100%;
+    height: auto;
+    background-color: black;">
 			<ul class="foot_icon" >
-				<li><a href="https://www.facebook.com/flyupent" target="_blank"><img class="event_img" src="/m/images/sns_1.png" alt="" style="height:30px;"/></a></li>
-				<li><a href="https://twitter.com/flyup_ent" target="_blank"><img class="event_img" src="/m/images/sns_2.png" alt="" style="height:30px;"/></a></li>
-				<li><a href="https://www.instagram.com/flyup_ent/" target="_blank"><img class="event_img" src="/m/images/sns_3.png" alt="" style="height:30px;"/></a></li>
-				<li><a href="" target="_blank"><img class="event_img" src="/m/images/sns_4.png" alt="" style="height:30px;"/></a></li>
+				<li><a href="https://www.facebook.com/flyupent" target="_blank"><img class="event_img" src="/m/images/sns_1.png" alt=""/></a></li>
+				<li><a href="https://twitter.com/flyup_ent" target="_blank"><img class="event_img" src="/m/images/sns_2.png" alt=""/></a></li>
+				<li><a href="https://www.instagram.com/flyup_ent/" target="_blank"><img class="event_img" src="/m/images/sns_3.png" alt=""/></a></li>
+				<li><a href="http://www.naver.com/" target="_blank"><img class="event_img" src="/m/images/sns_5.png" alt="" /></a></li>
+				<li><a href="" target="_blank"><img class="event_img" src="/m/images/sns_4.png" alt="" /></a></li>
 			</ul>
 		</div>
 		<div style=" clear:both; width:85%; text-align:center; margin:15px auto; ">
-			<p style="color:#e8e7e7;">(주)플라이업엔터테인먼트 / 06045 서울특별시 강남구 강남대로 132길 55(논현동) 광림빌딩 6층<br>
-			TEL. 02-516-5502 / FAX. 02-516-5503<br>
-			6F Gwangrim B/D 55, Gangnam-daero 132-gil, Gangnam-gu, Seoul, Korea<br>
-			<span style="color:#7f7f7f;">Copyright ⓒ 2015 flyup.co.kr. ALL RIGHTS RESERVED.</span></p>
+			<p style="color: rgb(164,164,164);">(주)플라이업엔터테인먼트 <br>
+			Copyright ⓒ 2015 FLYUP.CO.KR. ALL RIGHTS RESERVED.<br>
+			<br>
+
+			06045 서울특별시 강남구 강남대로 132길 55(논현동) 광림빌딩 6층<br>
+			TEL. 02-516-5502 | FAX. 02-516-5503<br>
+			6F GWANGRIM B/D 55, GANGNAM-DAERO 132-GIL<br> GANGNAM_GU, SEOUL, KOREA<br>
 		</div>
 	</div><!-- footer 종료 -->
 
