3ba39ed3984db9e2b009ed311b7b9e90fc3d11df
jangsang <episodejsh@naver.com>
2016-06-12 04:23:49 +0900

    on progress modification

--- subpage/audition/audition_01.php
+++ subpage/audition/audition_01.php
@@ -1,128 +1,29 @@
 <?php include($_SERVER["DOCUMENT_ROOT"]."/m/page_top.php"); ?>
 
-	<div class="sub_visual_05">
-		<div class="sub_top_tt">
-			Audition<br />
-			<div class="sub_top_tt_line"></div>
-			<div class="sub_top_m">
-			(주)플라이업엔터테인먼트<br>
-			오디션/채용안내
-			</div>
-		</div><!-- sub_top_tt 종료 -->
+	<div class="sub_audi_01">
 	</div><!-- sub_visual 종료 -->
 
-	
-	<div class="sub_con">
+
 		<div class="sub_contents">
-
-		<div class="audi_tap">
-			<div class="a_tap" style="background:#007bc7;">
-				<a href="/m/subpage/audition/audition_01.asp">Audition</a>
-			</div>
-			<div class="r_tap" style="background:#b9b9b9;">
-				<a href="/m/subpage/audition/audition_02.asp">Recruitment</a>
-			</div>
-		 </div>
-
-		<div class="box_out">
-			<div class="box_in">			 
-			  <div style="width:86%; margin:0px auto; line-height:200%;">
-				  
-				  <p class="audi_tt">
-					<span style="font-size:22px; color:#016eb2;">FLYUP ENTERTAINMENT</span>에서는
-					<span style="font-size:18px; color:#444444;">재능있는 신인 연기자</span>를 모집합니다.
-				  </p>
-				  <div class="audi_title" style="margin-top:25px;">
-				    오디션 안내
-			      </div>
-				  <p>
-					<strong>모집분야 : </strong>연기자<br>
-					<strong>모집기간 : </strong>수시 모집<br>
-					<strong>지원자격 : </strong>10세 이상 ~ 30세 이하의 끼와 재능을 갖춘 신인이라면 누구나, 단, 미성년자는
-					부모님 동의 필요. 
-				  </p>
-				  <div class="audi_title" style="margin-top:40px;">
-				    오디션 진행절차
-			      </div>
-				  <img src="/m/images/audi.jpg" style="width:100%;">
-				  <p style="margin-top:15px;">
-					<strong>01.오디션 지원</strong>(지원서, 사진, 영상 E-mail/우편 접수)<br>
-					<strong>02.1차 서류심사</strong>(합격자 개별 통보)<br>
-					<strong>03.2차 현장심사</strong>(지정연기, 자유연기 및 카메라테스트)<br>
-					<strong>04.3차 심층면접</strong>
-				  </p>
-				  <div class="audi_title" style="margin-top:40px;">
-				    오디션 지원방법
-			      </div>
-				  <p>
-					오디션 지원서를 다운로드하여 작성한 후, 사진+영상과 함께 보내주세요.<br>
-					<span style="color:#ff8400">오디션 지원서, 사진, 영상 모두 필수입니다!! 누락된 자료가 없는지 반드시 확인하고 보내주세요.</span>
-				  </p>
-				  <div class="audi_table">
-					  <table width="100%" border="0" cellspacing="0" cellpadding="0">
-						  <tr>
-							 <td width="30%" bgcolor="#f7f7f7" align="center"><strong>온라인(e-mail)</strong></td>
-							 <td width="70%">flyup.ent@hanmail.net</td>
-						  </tr>
-						  <tr>
-							 <td bgcolor="#f7f7f7" align="center"><strong>오프라인(우편)</strong></td>
-							 <td>서울시 강남구 논현동 42-9 광림빌딩 6층 플라이업엔터테인먼트
-							(도로명주소:서울시 강남구 강남대로 132길 55 광림빌딩 6층 
-							플라이업엔터테인먼트)</td>
-						  </tr>
-					  </table>
-				  </div><!-- audi_table-->
-				  <p style="color:#ff8400">1차 서류 심사 합격자에 한해 개별 통보해 드리며 전화 문의는 받지 않습니다. 
-				  </p>
+						<div class="audi_tap">
+							<div class="a_tap" style="background: rgba(129,129,129,1);">
+								<a href="/m/subpage/audition/audition_01.php">지 원 방 법</a>
+							</div>
+							<div class="r_tap" style="background:white;">
+								<a href="/m/subpage/audition/audition_02.php">유 의 사 항</a>
+							</div>
+						</div>
+					<div class="sub_con_audi">	
+		
 				  <div class="down_btn">
 					<a href="/m/subpage/audition/flyup_audition.docx" target="_blank">오디션 지원서 다운로드
 					<img src="/m/images/down.png" style="width:12px; position:relative; top:3px; left:3px;"></a>
 				  </div>
-				 <div class="sub_in_line"></div>
-				 <div class="audi_title" style="">
-				    오디션 지원시 주의사항
-			      </div>
-				  <p>
-				    오디션 지원서에 응시일자, 이름, 연락처, 나이는 반드시 기재해주세요.<br>
-					ㆍ남자는 군필 여부 기재 필수<br>
-					ㆍ나이는 '만 나이' 가 아닌, '연 나이' 로 기재<br><br>
-
-					e-mail 접수의 경우, 파일명과 메일제목은 "날짜_이름" 으로 반드시 통일해주세요.<br>
-					ㆍex)20150115_홍길동<br><br>
-
-					오디션 지원은 온라인/오프라인에 관계없이 한 번만 가능합니다.<br>
-					ㆍ단, 처음 신청할 때 자료가 누락되어 수정이 불가피한 경우에는 다시 작성하여 지원 가능<br>
-					ㆍ동일한 사진과 영상으로 중복 지원 불가
-				  </p>
-				  <div class="audi_title" style="margin-top:40px;">
-				    사진 첨부 방법
-			      </div>
-				  <p>
-				   ㆍ사진은 일반사진 또는 전문 프로필 사진의 원본으로 첨부 (최소 3장 이상)<br>
-				   ㆍ얼굴 클로즈업, 상반신, 전신 사진 각 한 컷씩 필수<br>
-				   ㆍ셀카, 포토샵, 캠, 스티커사진 등 보정된 사진은 심사에서 제외<br>
-				   ㆍ얼굴을 제대로 분간할 수 없는 사진은 심사에서 제외<br>
-				  <span style="color:#ff8400">사진 첨부 방법을 반드시 준수해주세요!! 부적합한 사진은 심사에서 제외됩니다.
-				  </span>
-				  </p>
-				  <div class="audi_title" style="margin-top:40px;">
-				    영상 첨부 방법
-			      </div>
-				  <p>
-					ㆍ자기소개 영상 또는 연기 영상 첨부 (2분 내외)<br>
-					ㆍ정면이 제대로 보이도록 카메라 각도와 높이에 유의할 것<br>
-					ㆍ셀카 각도, 어두운 조명은 피할 것<br>
-					ㆍ얼굴을 제대로 분간할 수 없는 영상은 심사에서 제외<br>
-					<span style="color:#ff8400">영상 첨부 방법을 반드시 준수해주세요!! 부적합한 영상은 심사에서 제외됩니다.</span>
-				  </p>
-			  
-			  </div>			
-			</div><!-- box_in-->
-		</div><!--box_out-->
+					</div><!-- sub_contents -->
 
 
-		</div><!-- sub_contents -->
-	</div><!-- sub_con -->
+		
+	
 
 	
 
