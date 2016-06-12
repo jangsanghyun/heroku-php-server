a23d11a5ed969deaea61a7e8f94f8c0caad261c7
jangsang <episodejsh@naver.com>
2016-06-13 01:10:29 +0900

    filmo

---
+++ subpage/filmo.php
@@ -0,0 +1,7 @@
+<?php include($_SERVER["DOCUMENT_ROOT"]."/m/page_top.php"); ?>
+
+	<div style="margin-top:25px;">
+        <img src="../images/filmo.png" width="100%">
+	</div>
+
+<?php include($_SERVER["DOCUMENT_ROOT"]."/m/page_foot.php"); ?>
