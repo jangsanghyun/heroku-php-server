5d05e58b45a10db71a091835575df59e2cde2f06
jason-hwang <wotjd1005@gmail.com>
2016-06-12 21:27:50 +0900

    Added Star, Star_Detail, Star Images

--- subpage/star/star.php
+++ subpage/star/star.php
@@ -1,65 +1,24 @@
 <?php include($_SERVER["DOCUMENT_ROOT"]."/m/page_top.php"); ?>
-
-	<div class="sub_visual_02">
-		<div class="sub_top_tt">
-			Star<br />
-			<div class="sub_top_tt_line"></div>
-			<div class="sub_top_m">
-			(주)플라이업엔터테인먼트<br>
-			소속 아티스트들
-			</div>
-		</div><!-- sub_top_tt 종료 -->
-	</div><!-- sub_visual 종료 -->
-
-
-	<div class="sub_con">
-		<div class="sub_contents">
-
-			<div class="star_p">
-				<table width="100%" border="0" cellspacing="0" cellpadding="0">
-<%
-	Sql = "Select * From board_photo order by idx desc"
-	Set rs = dbcon.execute(sql)
-
-	If Not(rs.eof) Then 
-
-		jj = 1 
-		DO UNTIL RS.EOF
-
-		idx		= RS("idx")
-		B_FILE0 = RS("B_FILE0")
-		B_FILE1 = RS("B_FILE1")
-		
-		If jj = 3 Then jj = 1
-		
-			If jj = 1 Then 
- %>					
-					<tr>
-						<td><a href="star_04.asp?idx=<%=idx%>"><img src="/upload_file/board_photo/<%=B_FILE0%>"></a></td>
-<%
-			Else 
-%>
-						<td><a href="star_04.asp?idx=<%=idx%>"><img src="/upload_file/board_photo/<%=B_FILE0%>"></a></td>
-					</tr>
-<%
-			End If
-			
-
-		jj = jj + 1
-
-		RS.MOVENEXT
-		Loop
-
-	End If
 	
-%>
-					
-				</table>
-			 </div>
-
-		</div><!-- sub_contents -->
-	</div><!-- sub_con -->
-
-	
+	<div style="margin:25px;">
+		<img src="../../images/contact_top.png" width="100%"/>
+	</div>
+	<div class="star-link-body">
+	    <div><a href="/m/subpage/star/star_detail.php?id=02"><img src="/m/images/03_1star__02.png" width="100%"></a></div>
+	    <div><a href="/m/subpage/star/star_detail.php?id=03"><img src="/m/images/03_1star__03.png" width="100%"></a></div>
+	    <div><a href="/m/subpage/star/star_detail.php?id=04"><img src="/m/images/03_1star__04.png" width="100%"></a></div>
+	    <div><a href="/m/subpage/star/star_detail.php?id=05"><img src="/m/images/03_1star__05.png" width="100%"></a></div>
+	    <div><a href="/m/subpage/star/star_detail.php?id=06"><img src="/m/images/03_1star__06.png" width="100%"></a></div>
+	    <div><a href="/m/subpage/star/star_detail.php?id=07"><img src="/m/images/03_1star__07.png" width="100%"></a></div>
+	    <div><a href="/m/subpage/star/star_detail.php?id=08"><img src="/m/images/03_1star__08.png" width="100%"></a></div>
+	    <div><a href="/m/subpage/star/star_detail.php?id=09"><img src="/m/images/03_1star__09.png" width="100%"></a></div>
+	    <div><a href="/m/subpage/star/star_detail.php?id=10"><img src="/m/images/03_1star__10.png" width="100%"></a></div>
+	    <div><a href="/m/subpage/star/star_detail.php?id=11"><img src="/m/images/03_1star__11.png" width="100%"></a></div>
+	    <div><a href="/m/subpage/star/star_detail.php?id=12"><img src="/m/images/03_1star__12.png" width="100%"></a></div>
+	    <div><a href="/m/subpage/star/star_detail.php?id=13"><img src="/m/images/03_1star__13.png" width="100%"></a></div>
+	    <div><a href="/m/subpage/star/star_detail.php?id=14"><img src="/m/images/03_1star__14.png" width="100%"></a></div>
+	    <div><a href="/m/subpage/star/star_detail.php?id=15"><img src="/m/images/03_1star__15.png" width="100%"></a></div>
+	    <div style="clear:both;"></div>
+	</div>
 
 <?php include($_SERVER["DOCUMENT_ROOT"]."/m/page_foot.php"); ?>
---
+++ subpage/star/star_detail.php
@@ -0,0 +1,13 @@
+<?php include($_SERVER["DOCUMENT_ROOT"]."/m/page_top.php"); ?>
+	
+	
+	<div style="margin:25px;">
+		<img src="../../images/contact_top.png" width="100%"/>
+		<hr/>
+	</div>
+
+	<div style="margin:25px;">
+        <img src="../../images/star_detail_<?php echo $_GET['id'];?>.jpg" width="100%">
+	</div>
+
+<?php include($_SERVER["DOCUMENT_ROOT"]."/m/page_foot.php"); ?>
